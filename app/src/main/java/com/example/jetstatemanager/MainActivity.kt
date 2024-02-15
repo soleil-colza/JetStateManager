a268b969e1a88082dfe8dc47ff395cb93cb07311
Hina Nakahira <soleil.colza@icloud.com>
2024-02-15 21:17:34 +0900

    basic wellness manager ig it's gonna be

---
+++ app/src/main/java/com/example/jetstatemanager/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.jetstatemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetstatemanager.ui.theme.JetStateManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetStateManagerTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    StateManager()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StateManager(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses",
+        modifier = modifier
+            .padding(16.dp)
+    )
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun StateManagerPreview() {
+    JetStateManagerTheme {
+        StateManager()
+    }
+}
---
+++ app/src/main/java/com/example/jetstatemanager/presentation/WellnessScreen.kt
@@ -0,0 +1,10 @@
+package com.example.jetstatemanager.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.jetstatemanager.StateManager
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    StateManager(modifier)
+}
